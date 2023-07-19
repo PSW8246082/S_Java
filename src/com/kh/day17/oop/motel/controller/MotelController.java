284de916d82bebf57b9c6944d10891bfcf9ef3c5
parkseonwoo <emer8246082@gmail.com>
2023-07-19 16:52:01 +0900

    [ADD] 23/07/19 OOP 프로그래밍 실습
    1. 모텔 관리 프로그램 v1.0
      -클래스 하나로 프로그래밍 하기
      -MVC 패턴 적용 후 클래스 나눠서 프로그래밍 하기
    2. Controller를 View를 이용하여 완전히 나눠보기

---
+++ src/com/kh/day17/oop/motel/controller/MotelController.java
@@ -0,0 +1,43 @@
+package com.kh.day17.oop.motel.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MotelController {
+	
+//	List<Room> rList;
+	List<Boolean> rList;
+	
+	public MotelController() {
+//		rList = new ArrayList<Room>();
+		rList = new ArrayList<Boolean>();
+		for(int i = 0; i < 10; i++) {
+//			rList.add(new Room());
+			rList.add(false);
+		}
+	}
+	
+	public List<Boolean> getAllRooms() {
+		return rList;
+	}
+	
+	public Boolean checkIn(int roomNo) {
+		Boolean flag = false;
+		if(!rList.get(roomNo-1)) {
+			rList.set(roomNo-1, true);
+			return !flag;
+		}
+		return flag;
+	}
+	
+	public Boolean checkOut(int chosenRoom) {
+		Boolean flag = false;
+		if(rList.get(chosenRoom-1)) {
+			rList.set(chosenRoom-1, false);
+			return !flag;
+		}
+		return flag;
+	}
+
+
+}
---
+++ src/com/kh/day17/oop/motel/run/MotelRun.java
@@ -0,0 +1,58 @@
+package com.kh.day17.oop.motel.run;
+
+import java.util.List;
+import java.util.Scanner;
+
+import com.kh.day17.oop.motel.controller.MotelController;
+import com.kh.day17.oop.motel.view.MotelView;
+
+public class MotelRun {
+
+	public static void main(String[] args) throws Exception{
+		
+		MotelView view = new MotelView();
+		MotelController controller = new MotelController();
+		Scanner sc = new Scanner(System.in);
+		int chosenRoom = 0;
+		Boolean changeYn = null;	// checkIn 또는 checkOut에 따라 성공여부를 리턴함
+									// checkIn,checkOut 성공시 true 리턴
+									// checkIn,checkOut 실패시 false 리턴
+		end : 
+		while(true) {
+			int choice = view.printMenu();
+			switch(choice) {
+			case 1 : 
+				chosenRoom = view.inputRoomNubmer("입실");
+				if(chosenRoom != -1) {
+					changeYn = controller.checkIn(chosenRoom);
+					view.roomCheckin(changeYn, chosenRoom);
+				}
+				Thread.sleep(500);
+				break;
+			case 2 : 
+				chosenRoom = view.inputRoomNubmer("퇴실");
+				if(chosenRoom != -1) {
+					changeYn = controller.checkOut(chosenRoom);
+					view.roomCheckOut(changeYn, chosenRoom);
+				}
+				Thread.sleep(500);	
+				break;
+			case 3 : 
+				List<Boolean> rList = controller.getAllRooms();
+				view.printAllRooms(rList);
+				Thread.sleep(1300);
+				break;
+			case 4 : 
+				System.out.println(".");
+				Thread.sleep(600);
+				System.out.println(".");
+				Thread.sleep(600);
+				System.out.println("프로그램을 종료합니다.");
+				break end;
+			}
+		}
+	
+	}
+
+}
+
